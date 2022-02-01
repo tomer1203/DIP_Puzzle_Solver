856a99684be248efc2fcffc4819ed9b442024f25
tomer1203 <tomer1shaked@gmail.com>
2022-02-01 18:03:38 +0200

    improved the segmentation and added royis feature detection

--- demos/segmentation_tomer.m
+++ demos/segmentation_tomer.m
@@ -1,4 +1,4 @@
-function [seg_img,overlay,extent] = segmentation_tomer(img,filt_size,extent_const)% filt_size = 3, extent_const = 0.5
+function [seg_img,overlay,extent] = segmentation_tomer(img,filt_size,extent_const)% filt_size = 5, extent_const = 0.3
 %     RGB=imread("..\img\pzl_6_2.jpg");
 %     imshow(RGB)
     % RGB = imread('kobi.png');
@@ -14,19 +14,21 @@ function [seg_img,overlay,extent] = segmentation_tomer(img,filt_size,extent_cons
     dial = imdilate(BW,strel("rectangle",[filt_size,filt_size]));
 %     solidity = bwpropfilt(dial, 'Solidity', [0, 0.5]);
     extent = bwpropfilt(dial, 'Extent', [0, extent_const]);
-    
-    % imshow(BW);
-    % imshow(dial);
-    % imshow(solidity);
-%     imshow(extent);
-    % imshow(imfill(extent,"holes"));
-    % imshow(imfill(imfill(solidity,'holes'),"holes"));
-    I = im2gray(im2single(img));
-    evolved = activecontour(I,extent,200,"Chan-vese","ContractionBias",-0.1,"SmoothFactor",0.6);
-    filled = imfill(evolved,"holes");
+    ext_dial = imdilate(extent,strel("rectangle",[20,20]));
+
+
+% imshow(area_filter);
+% imshow(imfill(area_filter,"holes"));
+% imshow(imfill(area_filter,8,"holes"));
+% imshow(imfill(extent,"holes"));
+% imshow(imfill(imfill(solidity,'holes'),"holes"));
+% I = im2gray(im2single(img));
+% evolved = activecontour(I,ext_dial,10,"Chan-vese","ContractionBias",-0.1);
+mask = imfill(double(ext_dial),8,"holes");
+mask = imerode(mask,strel("rectangle",[10,10]));
 %     imshow(evolved);
 %     imshow(filled);
-    overlay = labeloverlay(img,filled);
-    seg_img = filled;
+    overlay = labeloverlay(img,mask);
+    seg_img = mask;
 %     imshow(C);
 end

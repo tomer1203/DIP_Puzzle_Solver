016fb710f5aa315230c832d1d0b8ad7dd52fb586
tomer1203 <tomer1shaked@gmail.com>
2022-02-02 17:51:40 +0200

    segmentation improvments

--- demos/cut_images.m
+++ demos/cut_images.m
@@ -13,9 +13,9 @@ function imgCell = cut_images(img,mask,N,padding)
         
         temp2 = img.*cast(temp,"like",img);
         % add padding to the edges of the images
-        r_low = max(min(r)-padding,0);
+        r_low = max(min(r)-padding,1);
         r_high = min(max(r)+padding,size(temp,1));
-        c_low = max(min(c)-padding,0);
+        c_low = max(min(c)-padding,1);
         c_high = min(max(c)+padding,size(temp,2));
 
         img_cut=temp2(r_low:r_high,c_low:c_high,:);
@@ -27,37 +27,43 @@ function imgCell = cut_images(img,mask,N,padding)
         lines = houghlines(BWedge,T,R,peaks);
         angles = [lines.theta]';
         % find the two average angles
-        if (length(angles)== 1)% in case there is only one line
-            two_angles = [angles(1),angles(1)];
+        if (isempty(angles) == 0)
+
+            if (length(angles)== 1)% in case there is only one line
+                two_angles = [angles(1),angles(1)];
+            else
+                [~,two_angles,~,~] = kmeans(angles,2);
+            end
+            % match to the closest angle
+            a1 = mod(two_angles(1),90);
+            if (a1>45)
+                a1 = a1-90;
+            end
+            img_rot = imrotate(img_cut,a1,"crop");
         else
-            [~,two_angles,~,~] = kmeans(angles,2);
+            disp("found no lines!")
+            img_rot = img_cut;
         end
-        % match to the closest angle
-        a1 = mod(two_angles(1),90);
-        if (a1>45)
-            a1 = a1-90;
-        end
-        img_rot = imrotate(img_cut,a1,"crop");
         imgCell{i} = img_rot;
-%         subplot(2,2,i);
+%         subplot(5,3,i);
 %         imshow(imgCell{i});
-    %     hold on
-    %     max_len = 0;
-    %     for k = 1:length(lines)
-    %        xy = [lines(k).point1; lines(k).point2];
-    %        plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-    %     
-    %        % Plot beginnings and ends of lines
-    %        plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-    %        plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
-    %     
-    %        % Determine the endpoints of the longest line segment
-    %        len = norm(lines(k).point1 - lines(k).point2);
-    %        if ( len > max_len)
-    %           max_len = len;
-    %           xy_long = xy;
-    %        end
-    %     end
+%         hold on
+%         max_len = 0;
+%         for k = 1:length(lines)
+%            xy = [lines(k).point1; lines(k).point2];
+%            plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+%         
+%            % Plot beginnings and ends of lines
+%            plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+%            plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+%         
+%            % Determine the endpoints of the longest line segment
+%            len = norm(lines(k).point1 - lines(k).point2);
+%            if ( len > max_len)
+%               max_len = len;
+%               xy_long = xy;
+%            end
+%         end
         
     end
 end
---
+++ segmentation.m
@@ -0,0 +1,34 @@
+% img= Normalized image in grayscale for segmentation.
+% dialation_size = increases the boldness of the lines def = 2
+% dialation_size2 = increases the boldness of the lines def = 5
+% extent_filter= filter segments according to their density(extent) def=0.3
+% center_size= a size representing how large to make the center of each
+% puzzle piece which is later used to seperate glued puzzle pieces def=60
+function [seg_img,puz_edges] = segmentation(img,dialtion_size1,dialation_size2,extent_filter,center_size)%def=(img,2,5,0.3,60)
+    % instead of using edges we use image gradients since they are more
+    % consistent 
+    BW= imgradient(img)>0.2;
+
+    % increase size of lines so that all the edge lines will get consistent
+    dial = imdilate(BW,strel("rectangle",[dialtion_size1,dialtion_size1]));
+
+    % filter out the edges according to their extent. this removes most of
+    % the noise
+    extent = bwpropfilt(dial, 'Extent', [0, extent_filter]);
+
+    % another final dilation to hopefully close all puzzle lines
+    puz_edges = imdilate(extent,strel("rectangle",[dialation_size2,dialation_size2]));
+
+    % fill the shape edges to get a good segmentation
+    mask = imfill(double(puz_edges),8,"holes");
+    % possible to add erosion here
+    % mask = imerode(mask,strel("rectangle",[10,10]));
+    
+    % this part cuts off glued pieces.
+    distTrans = -bwdist(~mask);
+    extended_dist = imextendedmin(distTrans,center_size);
+    D2 = imimposemin(distTrans,extended_dist);
+    Ld2 = watershed(D2);
+    seg_img = mask;
+    seg_img(Ld2 == 0) = 0;
+end

c23f41622db53eec597b02e6decc8101ab07675e
royiz <royiz@post.bgu.ac.il>
2022-02-14 17:11:21 +0200

    Create prespective_transformation.m

---
+++ prespective_transformation.m
@@ -0,0 +1,8 @@
+function convrt_img=prespective_transformation(I)
+[x,y]=find_corner(I); 
+[N,M,~]=size(I);
+fixedPoints = [0 0; M 0; M N; 0 N];
+tform = fitgeotrans([x(1) y(1); x(2) y(2); x(3) y(3); x(4) y(4)],fixedPoints,'projective');
+convrt_img = imwarp(I,tform,'OutputView',imref2d(size(I)));
+convrt_img=flipdim(convrt_img,2);
+end

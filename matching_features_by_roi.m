cd27511806228f7be8b45f9e9c8ad54539cb492e
royiz <royiz@post.bgu.ac.il>
2022-02-19 14:51:43 +0200

    Update matching_features_by_roi.m

--- matching_features_by_roi.m
+++ matching_features_by_roi.m
@@ -1,41 +1,37 @@
-% input locations-location to check
-%       scale=[width height]
-%       pieces
-%       grid_img
-% output location realibilitirs matrix
-function location_realibilitirs_matrix= matching_features_by_roi(locations_matrix,scale,pieces,img_grid)
-MetricThreshold=100;               %default 1000. decerase the tradhold- more features
-NumOctaves=4;                       %default 3 . recommend: 1-4 . increase the numOctuves- more features
-NumScaleLevels=6;                   %default 4. recommend  3-6 .increase the numScaleLevel- more features 
-MatchThreshold_parameter=100;                 % default is 1, increas help to find more match features
-
-pieces = double(im2gray(piece));
-pieces = (pieces - min(pieces(:)))/(max(pieces(:)) - min(pieces(:)));
-gray_grid = double(im2gray(img_grid));
-gray_grid = (gray_grid - min(gray_grid(:)))/(max(gray_grid(:)) - min(gray_grid(:)));
-[width height]=scale(1),scale(2);
-[num_row,num_col]=size(locations_matrix);
-
-
+% input  location_row_empty
+% R      location_col_empty
+%        pieces= the pieces to check
+%        grid_img
+%        num_row,num_col
+% output location_new of the piece
+%        reliability_new of the piece
+function [location_new,reliability_new]= matching_features_by_roi(location_row_empty,location_col_empty,pieces,img_grid,num_row,num_col)
+app=false;
 %find the empty locations:
-location_row=cell(1);
-location_col=cell(1);
-for i=1:row;
-    for j=1:col;
-        if location_matrix_features{i,j}==0
-            location_row{1}=[location_row{1},i];
-            location_col{1}=[location_col{1},j];
-        end
-    end
-end
 % number of problematic locaions:
-number_of_location=length(location_col{1});
-%
-%surf feature for the pieces:
-points1 = detectSURFFeatures(pieces,"MetricThreshold",MetricThreshold,"NumOctaves",NumOctaves,"NumScaleLevels",NumScaleLevels);
-[f1,vpts1] = extractFeatures(pieces,points1,"Method","SURF");
-%surf features for the 
-for i=1:number_of_location;
+number_of_location=length(location_col_empty);
+if(number_of_location~=0) % more empty location
+    relability_vec=zeros(1,number_of_location);
+    location_vec_x=zeros(1,number_of_location);
+    location_vec_y=zeros(1,number_of_location);
+    for i=1:number_of_location;
+        k_col=location_col_empty(i);
+        j_row=location_row_empty(i);
+        width=floor(m/num_col);
+        height=floor(n/num_row);
+        x=(k_col-1)*floor(m/num_col)+1;
+        y=(j_row-1)*floor(n/num_row)+1;
+        R_roi=[x y width height];
+        [A,relability_vec(i)]=matching_features(pieces,img_grid,num_row,num_col,app,R_roi);
+        [location_vec_x(i),location_vec_y(i)]=A;
+        clear A;
+    end
+    reliability_new=max(relability_vec);
+    cordinate=find(relability_vec=reliability_new);
+    location_new=zeros(2,1);
+    location_new(1)=location_vec_x(cordinate);
+    location_new(2)=location_vec_y(cordinate);
+    
 
 end
 

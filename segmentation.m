dc63f5d1f18993a12df9d2670d165f297bbd526d
royiz <royiz@post.bgu.ac.il>
2022-02-20 12:14:42 +0200

    Update segmentation.m

--- segmentation.m
+++ segmentation.m
@@ -6,6 +6,7 @@
 % puzzle piece which is later used to seperate glued puzzle pieces def=60
 function [seg_img,puz_edges] = segmentation(img,dialtion_size1,dialation_size2,extent_filter,center_size)%def=(img,2,5,0.3,60)
     % instead of using edges we use image gradients since they are more
+    show=false;
     % consistent 
     BW= imgradient(img)>0.2;
 %     figure;
@@ -25,9 +26,17 @@ function [seg_img,puz_edges] = segmentation(img,dialtion_size1,dialation_size2,e
     mask = imfill(double(puz_edges),8,"holes");
     % possible to add erosion here
     mask = imerode(mask,strel("rectangle",[dialation_size2,dialation_size2]));
-%     figure;
-%     imshow(mask);
-
+    if(show)
+        figure;
+        imshow(BW);
+        title("BW")
+        figure;
+        imshow(extent);
+        title("extent")
+        figure;
+        imshow(mask);
+        title("MASK")
+    end
     % this part cuts off glued pieces.
     distTrans = -bwdist(~mask);
     extended_dist = imextendedmin(distTrans,center_size);

69d9e330d211b95991ed295de5e69569221665ab
tomer1203 <tomer1shaked@gmail.com>
2022-02-21 10:40:17 +0200

    added find piece label blueprint

---
+++ find_piece_label.m
@@ -0,0 +1,7 @@
+% imgCell      = images organized by label
+% featuresCell = features organized by label
+% vptsCell     = vpts organized by label
+function [label,reliability] = find_piece_label(piece,imgCell,featuresCell,vptsCell)
+    label      =1;
+    reliability=1;
+end

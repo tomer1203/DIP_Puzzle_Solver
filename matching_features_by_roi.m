709a9ff28515ea4253b2e5f178b0f77f2e85f5d9
royiz <royiz@post.bgu.ac.il>
2022-02-21 15:25:49 +0200

    Update matching_features_by_roi.m

--- matching_features_by_roi.m
+++ matching_features_by_roi.m
@@ -5,7 +5,7 @@
 %        num_row,num_col
 % output location_new of the piece
 %        reliability_new of the piece
-function [location_new,reliability_new]= matching_features_by_roi(location_row_empty,location_col_empty,pieces,img_grid,num_row,num_col)
+function [location_new,reliability_new]= matching_features_by_roi(location_row_empty,location_col_empty,pieces,img_grid,num_row,num_col,app)
 app=false;
 [n,m,~] = size(img_grid);
 %find the empty locations:
@@ -15,7 +15,9 @@ if(number_of_location~=0) % more empty location
     relability_vec=zeros(1,number_of_location);
     location_vec_x=zeros(1,number_of_location);
     location_vec_y=zeros(1,number_of_location);
-    for i=1:number_of_location;
+    f=[];
+    vtps=[];
+    for i=1:number_of_location
         k_col=location_col_empty(i);
         j_row=location_row_empty(i);
         width=floor(m/num_col);
@@ -23,16 +25,22 @@ if(number_of_location~=0) % more empty location
         x=(k_col-1)*floor(m/num_col)+1;
         y=(j_row-1)*floor(n/num_row)+1;
         R_roi=[x y width height];
-        [A,relability_vec(i)]=matching_features_surf(pieces,img_grid,num_row,num_col,app,R_roi);
-        location_vec_x(i)=A(1);
-        location_vec_y(i)=A(2);
-        clear A;
+        points2_flag=0;
+        [~, ~,f1,vtps1]= matching_features(pieces,img_grid,num_row,num_col,points2_flag,app,0,0,R_roi);
+        f=[f ; f1];
+        if(i==1)
+        vtps=vtps1;
+        else
+       vtps=[vtps;vtps1];
+        
     end
-    reliability_new=max(relability_vec);
-    cordinate=find(relability_vec==reliability_new);
-    location_new=zeros(2,1);
-    location_new(1)=location_vec_x(cordinate);
-    location_new(2)=location_vec_y(cordinate);
+    [location_new,reliability_new,~,~]= matching_features(pieces,img_grid,num_row,num_col,1,app,f,vtps,0);
+    
+%     reliability_new=max(relability_vec);
+%     cordinate=find(relability_vec==reliability_new);
+%     location_new=zeros(2,1);
+%     location_new(1)=location_vec_x(cordinate);
+%     location_new(2)=location_vec_y(cordinate);
     
 
 end

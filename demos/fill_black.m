31bf246b9761b682aa2d1387e1aa31cae155c87d
tomer1203 <tomer1shaked@gmail.com>
2022-03-16 15:29:51 +0200

    Final Submission!

--- demos/fill_black.m
+++
@@ -1,5 +0,0 @@
-% as long as there are more then threshold black pixels in a square of size
-% filt_size * filt_size the output pixel would be black
-function filled_img = fill_black(img,filt_size,threshold)
-    filled_img = 1 - fill_white(1-img,filt_size,threshold);
-end
--- demos/save_img.m
+++
@@ -1,8 +0,0 @@
-cam = webcam(2);
-% cam.Brightness = ;
-preview(cam);
-pause(3);
-img = snapshot(cam);
-% corrected_img =prespective_transformation(img);
-imshow(img);
-imwrite(img,"temp.jpg");
